508ecd3413524d97df472993a1b5f48591dfa52d
Francisco Carnelose <124386850+fcarnelose@users.noreply.github.com>
2025-07-29 16:27:12 -0300

    29/07

---
+++ assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Matemática',
+    'O que é MMC?',
+    'É o Mínimo Múltiplo Comum'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a Capital da Inglaterra?',
+    'A capital da Inglaterra é Londres'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual o Melhor Time do Mundo?',
+    'É o ***Corinthians***'
+)
