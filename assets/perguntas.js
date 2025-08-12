d401717649c6e3e5d87f097ed4288dac0d957f3c
Francisco Carnelose <124386850+fcarnelose@users.noreply.github.com>
2025-08-12 16:28:01 -0300

    12/08

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -13,6 +13,23 @@ criaCartao(
 criaCartao(
     'Esportes',
     'Qual o Melhor Time do Mundo?',
-    'É o ***Corinthians***'
+    'É o Corinthians'
+)
+
+criaCartao(
+    'Games',
+    'Qual o Jogo mais aguardado de 2026?',
+    'GTA VI'
+)
+
+criaCartao(
+    'Física',
+    'Quem inventou a lâmpada?',
+    'Thomas Edison'
+)
+criaCartao(
+    'Astronomia',
+    'Qual o maior planeta do sistema solar?',
+    'Júpiter'
 )
 
